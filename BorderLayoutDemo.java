dcd366a3eed1139c96575c3229bdf31b4a8e05c8
jordan james olive <jolive@AUS213L12.byui.edu>
2014-02-13 13:10:14 -0700

    working

--- BorderLayoutDemo.java
+++ BorderLayoutDemo.java
@@ -2,20 +2,27 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+
 public class BorderLayoutDemo
    extends JFrame
+   implements ItemListener
 {
    public static void main(String[] args)
    {
       new BorderLayoutDemo().setVisible(true);
+      
    }
 
+   JCheckBox mCheck;
+   
    public BorderLayoutDemo()
    {
       super("Border Layout Demo");
       setFont(new Font("Arial",1,20));
       setLayout(new BorderLayout());
 
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      /*
       addWindowListener(new WindowAdapter()
          {
             public void windowClosing(WindowEvent e)
@@ -23,44 +30,44 @@ public class BorderLayoutDemo
                System.exit(0);
             }
          });
+      */
       
-      JFrame frame = new JFrame();
+      //JFrame frame = new JFrame();
       
       JButton east = new JButton("East");
       JButton west = new JButton("West");
       JButton south = new JButton("South");
       JButton north = new JButton("North");
-       JButton center = new JButton("Center");
-
-      frame.getContentPane().add(BorderLayout.EAST, east);
-      frame.getContentPane().add(BorderLayout.WEST, west);
-      frame.getContentPane().add(BorderLayout.SOUTH, south);
-      frame.getContentPane().add(BorderLayout.NORTH, north);
-      frame.getContentPane().add(BorderLayout.CENTER, center);
-
-      frame.setSize(300,300);
-      frame.setVisible(true);
-   }
-}
-   /*
-   public void go()
-   {
-      JFrame frame = new JFrame();
+      JButton center = new JButton("Center");
       
-      Jbutton east = new Jbutton("East");
-      Jbutton west = new Jbutton("West");
-      Jbutton south = new Jbutton("South");
-      Jbutton north = new Jbutton("North");
+      mCheck = new JCheckBox("Goes to 11");
+      mCheck.addItemListener(this);
+          
 
-      frame.getContentPane().add(BoarderLayout.EAST, east);
-      frame.getContentPane().add(BoarderLayout.WEST, west);
-      frame.getContentPane().add(BoarderLayout.SOUTH, south);
-      frame.getContentPane().add(BoarderLayout.NORTH, north);
 
-      frame.setSize(300,300);
-      frame.setVisible(true);
+      getContentPane().add(BorderLayout.EAST, east);
+      getContentPane().add(BorderLayout.WEST, west);
+      getContentPane().add(BorderLayout.SOUTH, south);
+      getContentPane().add(BorderLayout.NORTH, north);
+      getContentPane().add(BorderLayout.CENTER, mCheck);
+
+      
+      setSize(300,300);
+      // setVisible(true);
    }
-   */
-         
+ 
+   public void itemStateChanged(ItemEvent ev)
+   {
+      String onOrOff = "off";
+      if (mCheck.isSelected())
+      {
+         onOrOff = "on";
+         System.out.println("Check box is " + onOrOff);
+      }
+          
+   }
+
+}
+ 
       
   
